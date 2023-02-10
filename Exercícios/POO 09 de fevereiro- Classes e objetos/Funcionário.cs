22c4a7be91b60b45b92f6519874cc49626e5e711
KelvinFS04 <kelvin_fs04@hotmail.com>
2023-02-09 22:15:21 -0300

    Aula de POO 09/02
    Classes e Objetos

---
+++ Exercícios/POO 09 de fevereiro- Classes e objetos/Funcionário.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_09_de_fevereiro__Classes_e_objetos
+{
+    public class Funcionário
+    {
+        public int codigo;
+        public string nome;
+        public DateTime Nascimento;
+        
+
+        public int CalculaIdade()
+        {
+            int idade = (int)DateTime.Now.Subtract(Nascimento).TotalDays / 365;
+            return idade;
+        }
+    }
+}
---
+++ Exercícios/POO 09 de fevereiro- Classes e objetos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_09_de_fevereiro__Classes_e_objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //entrada de dados
+            Funcionário funcionário = new Funcionário();
+            Console.WriteLine("Digite o código");
+            funcionário.codigo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o nome");
+            funcionário.nome = Console.ReadLine();
+            Console.WriteLine("Digite a data de nascimento");
+            funcionário.Nascimento = Convert.ToDateTime(Console.ReadLine());
+
+            Console.WriteLine("\n\n Dados cadastrados\n\n");
+            Console.WriteLine( $"Código: {funcionário.codigo} \nNome: {funcionário.nome} " +
+                $"\nNascimento: {funcionário.Nascimento.ToShortDateString()}" +
+                $"\nIdade: {funcionário.CalculaIdade()}" );
+            Console.ReadKey();
+
+        }
+    }
+}
