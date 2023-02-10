5aef53adcf4277e519814a331f4bc8acac2e959f
KelvinFS04 <kelvin_fs04@hotmail.com>
2023-02-09 22:42:21 -0300

    Término primeira parte da aula

--- Exercícios/POO 09 de fevereiro- Classes e objetos/Funcionário.cs
+++ Exercícios/POO 09 de fevereiro- Classes e objetos/Funcionário.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,60 @@ namespace POO_09_de_fevereiro__Classes_e_objetos
 {
     public class Funcionário
     {
-        public int codigo;
-        public string nome;
-        public DateTime Nascimento;
-        
+        private int codigo;
+        private string nome;
+        private DateTime nascimento;
+        private string cpf;
 
-        public int CalculaIdade()
+
+        public void setCodigo(int valor)
         {
-            int idade = (int)DateTime.Now.Subtract(Nascimento).TotalDays / 365;
+            if (valor <= 0)
+            {
+                throw new Exception("Código não pode ser negativo");
+            }
+            else
+            {
+                codigo = valor;
+            }
+        }
+
+        //Método de uma linha tipo "arrow" 
+        //public int getCodigo() => codigo;
+        public int getCodigo() 
+        {
+            return codigo;
+        }
+
+
+        public void setNome(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new Exception("Informe o nome");
+            }
+            else
+            {
+                this.nome = nome;
+            }
+        }
+        public string getNome() => nome;
+
+        public void setDataNascimento(DateTime data) 
+        {
+            if (data > DateTime.Now) 
+            {
+                throw new Exception("Pessoa nem nasceu ainda...");
+            }
+            else
+            {
+                nascimento= data;
+            }
+        }
+
+        private int CalculaIdade()
+        {
+            int idade = (int)DateTime.Now.Subtract(nascimento).TotalDays / 365;
             return idade;
         }
     }
--- Exercícios/POO 09 de fevereiro- Classes e objetos/Program.cs
+++ Exercícios/POO 09 de fevereiro- Classes e objetos/Program.cs
@@ -10,18 +10,20 @@ namespace POO_09_de_fevereiro__Classes_e_objetos
     {
         static void Main(string[] args)
         {
-            //entrada de dados
+            //entrada de dados pré privatização dos atributos
             Funcionário funcionário = new Funcionário();
             Console.WriteLine("Digite o código");
             funcionário.codigo = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Digite o nome");
             funcionário.nome = Console.ReadLine();
             Console.WriteLine("Digite a data de nascimento");
-            funcionário.Nascimento = Convert.ToDateTime(Console.ReadLine());
+            funcionário.nascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Digite o cpf");
+            funcionário.cpf = Console.ReadLine();
 
             Console.WriteLine("\n\n Dados cadastrados\n\n");
             Console.WriteLine( $"Código: {funcionário.codigo} \nNome: {funcionário.nome} " +
-                $"\nNascimento: {funcionário.Nascimento.ToShortDateString()}" +
+                $"\nNascimento: {funcionário.nascimento.ToShortDateString()}" +
                 $"\nIdade: {funcionário.CalculaIdade()}" );
             Console.ReadKey();
 
