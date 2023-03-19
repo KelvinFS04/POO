651af13732378308a4ebd2f35b8bcdd0481801b9
KelvinFS04 <kelvin_fs04@hotmail.com>
2023-03-19 18:36:48 -0300

    Exercícios valendo nota feitos
    8 horas de estudo, revisão e programação sem commitar

---
+++ Aulas/Sobrecarga de método/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga_de_método
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine("1");
+            Console.WriteLine(1.0);
+            Console.WriteLine(1.0000000000000000000000009);
+            
+        }
+    }
+}
